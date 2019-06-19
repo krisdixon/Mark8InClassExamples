bbb0054e5fee0e85a92bda377092d75a782d8dae
Kris <krisdixon@yahoo.com>
2019-06-19 12:24:13 -0500

    Add project files.

---
+++ Mark8InClassExamples/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Mark8InClassExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string firstName = "Kris";
+            int favoriteNumber = 2;
+            double pi = 3.14;
+            bool isMarried = true;
+
+            
+
+            if (isMarried == true)
+            {
+                Console.WriteLine("you are married");
+            }
+        
+
+       
+        }
+
+
+
+    }
+}
